037a70773f3e05230eccda9d371b68ac02f6777b
Felipe-53 <felipeandresb97@gmail.com>
2023-05-26 18:03:41 -0300

    parsing config

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::{env, fs};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let config = Config::new(&args);
+
+    let contents = fs::read_to_string(config.file_path).expect("Could not read file in path");
+}
+
+struct Config<'a> {
+    query: &'a String,
+    file_path: &'a String,
+}
+
+impl Config<'_> {
+    fn new(args: &[String]) -> Config {
+        let query = &args[1];
+        let file_path = &args[2];
+
+        Config { query, file_path }
+    }
 }
